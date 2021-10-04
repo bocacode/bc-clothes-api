ed60a88cae35eb7c051df284ca78de472c52228b
Darie Dorlus <tdorluss@gmail.com>
2021-10-04 16:56:34 -0400

    added crud capability to products

--- src/products.js
+++ src/products.js
@@ -1,10 +1,13 @@
 // connect to the database
 const { connectDb } = require('./db')
 
+const CLOTHES_COLLECTION = "clothes"
+
+const db = connectDb()
+
 // get all products
 exports.getAllProducts = (req, res) => {
-  const db = connectDb()
-  db.collection('clothes').get()
+  db.collection(CLOTHES_COLLECTION).get()
     .then(collection => {
       const products = collection.docs.map(doc => {
         let product = doc.data()
@@ -17,12 +20,58 @@ exports.getAllProducts = (req, res) => {
 }
 
 // get a single product
+exports.getProductByID = (req, res) => {
+  const { id }  = req.params
+
+  db.collection(CLOTHES_COLLECTION).doc(id).get()
+  .then(doc => {
+    let product = doc.data()
+    product.id = doc.id
+    res.send(product)
+  })
+  .catch(error => res.status(500).send(error))
+}
 
 
 // create a new product
+exports.createProduct = (req, res) => {
+  
+  const newProduct  = req.body
+
+  db.collection(CLOTHES_COLLECTION).add(newProduct)
+  .then(docRef => {
+    let product = req.body
+    product.id = docRef.id
+    res.send(product)
+  })
+  .catch(error => res.status(500).send(error))
+
+}
 
 
 // update a single product
 
+exports.updateProduct = (req, res) => {
+  let product = req.body
+  const { id } = req.params
+
+  db.collection(CLOTHES_COLLECTION).doc(id).update(product)
+  .then(() => this.getProductByID(req, res))
+  .catch(error => res.status(500).send(error))
+
+}
+
 
 // delete a single product
+
+exports.deleteProduct = (req, res) => {
+  const { id } = req.params
+
+  db.collection(CLOTHES_COLLECTION).doc(id).delete()
+  .then(timeStamp => {
+    res.status(200).send(`Product ${id} nuked at ${timeStamp}`)
+  })
+  .catch(error => res.status(500).send(error))
+
+
+}
